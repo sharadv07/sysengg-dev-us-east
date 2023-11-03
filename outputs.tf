c0934dc50b57272969d341d9d7605792e7f3f523
shardv07 <sharad.vidyapathy@gmail.com>
2023-11-02 23:35:18 -0400

    added outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "resource_group_id" {
+  description = "RG ID"
+  value       = azurerm_resource_group.appgrp.id
+}
+
+output "resource_group_name" {
+  description = "Resource Group Name"
+  value       = azurerm_resource_group.appgrp.name
+}
+
+output "resource_group_location" {
+  description = "Resource Group Location"
+  value       = azurerm_resource_group.appgrp.location
+}
